8675177db5658afb728bc6d1e7e9cc54b7eadc28
Romanova Ekaterina <lyynakat@gmail.com>
2019-06-20 20:15:07 +0300

    добавление теста на удаление группы

--- fixture/group.py
+++ fixture/group.py
@@ -28,6 +28,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #delete group
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
     def open_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
---
+++ test/test_del_group.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+def test_delete_first_group(app):
+        app.session.login("admin", "secret")
+        app.group.delete_first_group()
+        app.session.logout()
