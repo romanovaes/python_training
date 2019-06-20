84f180e9fbc9521b16634187ed8e2d373cef790e
Romanova Ekaterina <lyynakat@gmail.com>
2019-06-20 20:43:08 +0300

    изменние параметра закрытия браузера

--- fixture/session.py
+++ fixture/session.py
@@ -8,6 +8,7 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
 
     def login(self,user_name, password):
         wd = self.app.wd
