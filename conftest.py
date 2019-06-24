4112fc5c5d9472807a7b36fda9520c1fa5542aa5
Romanova Ekaterina <lyynakat@gmail.com>
2019-06-24 20:53:11 +0300

    создание интеллектуальной фикстуры

--- conftest.py
+++ conftest.py
@@ -2,11 +2,22 @@
 import pytest
 from fixture.application import Application
 
+fixture=None
 
 @pytest.fixture(scope="session")
 def app(request):
-    fixture=Application()
-    fixture.session.login("admin", "secret")
+    global fixture
+    if fixture is None:
+        fixture=Application()
+        fixture.session.login("admin", "secret")
+    else:
+        if not fixture.is_valid():
+            fixture=Application()
+            fixture.session.login("admin", "secret")
+    return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
