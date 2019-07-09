9749dfd256c5c995d22c235342b7b70f52e7c6d2
Romanova Ekaterina <lyynakat@gmail.com>
2019-07-09 21:53:26 +0300

    добавление проверки равенства списков в тест добавления группы

--- model/group.py
+++ model/group.py
@@ -1,3 +1,4 @@
+from sys import maxsize
 
 
 class Group:
@@ -11,4 +12,11 @@ class Group:
         return '%s:%s' %(self.id, self.name)
 
     def __eq__(self, other):
-        return self.name==other.name and self.id==other.id
+        return self.name==other.name and (self.id==other.id or self.id is None or other.id is None)
+
+
+    def id_or_max(self):
+                if self.id:
+                        return int(self.id)
+                else:
+                        return maxsize
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
+from sys import maxsize
 from model.group import Group
 
 def test_add_group(app):
         old_group=app.group.get_group_list()
-        app.group.create(Group("new", "new", "new"))
+        group=Group("new", "new", "new")
+        app.group.create(group)
         new_group=app.group.get_group_list()
         assert len(old_group)+1 == len(new_group)
+        old_group.append(group)
+        sorted(old_group, key=Group.id_or_max)==sorted(new_group, key=Group.id_or_max)
+
 
 def test_add_group_empty(app):
         old_group=app.group.get_group_list()
-        app.group.create(Group("", "", ""))
+        group=Group("", "", "")
+        app.group.create(group)
         new_group=app.group.get_group_list()
         assert len(old_group)+1 == len(new_group)
+        old_group.append(group)
+        sorted(old_group, key=Group.id_or_max)==sorted(new_group, key=Group.id_or_max)
+
 
 
