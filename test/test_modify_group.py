935e66eaf227902a1c718cd4881251aca4b0d0d4
Romanova Ekaterina <lyynakat@gmail.com>
2019-07-09 22:02:51 +0300

    добавление проверки равенства списков в тест модификации группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,15 +5,19 @@ def test_modify_group_name(app):
         if app.group.count()==0:
                 app.group.create(Group(name="Тест модификации name"))
         old_group=app.group.get_group_list()
-        app.group.modify_first_group(Group(name="new name"))
+        group=Group(name="new name1")
+        group.id=old_group[0].id
+        app.group.modify_first_group(group)
         new_group=app.group.get_group_list()
         assert len(old_group) == len(new_group)
+        old_group[0]=group
+        sorted(old_group, key=Group.id_or_max)==sorted(new_group, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-        if app.group.count()==0:
-                app.group.create(Group(name="Тест модификации header"))
-        old_group=app.group.get_group_list()
-        app.group.modify_first_group(Group(header="new header"))
-        new_group=app.group.get_group_list()
-        assert len(old_group) == len(new_group)
+#def test_modify_group_header(app):
+        #if app.group.count()==0:
+               # app.group.create(Group(name="Тест модификации header"))
+       # old_group=app.group.get_group_list()
+       # app.group.modify_first_group(Group(header="new header"))
+       # new_group=app.group.get_group_list()
+       # assert len(old_group) == len(new_group)
