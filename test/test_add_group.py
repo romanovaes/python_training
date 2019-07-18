cb1187774be21ef57a5b6d2daf17b3b8ebfa08c6
Romanova Ekaterina <lyynakat@gmail.com>
2019-07-18 20:25:02 +0300

    добавление кеширование в тест на добавление группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,20 +6,20 @@ def test_add_group(app):
         old_group=app.group.get_group_list()
         group=Group("new", "new", "new")
         app.group.create(group)
+        assert len(old_group)+1 == app.group.count()
         new_group=app.group.get_group_list()
-        assert len(old_group)+1 == len(new_group)
         old_group.append(group)
         assert sorted(old_group, key=Group.id_or_max)==sorted(new_group, key=Group.id_or_max)
 
 
-def test_add_group_empty(app):
-        old_group=app.group.get_group_list()
-        group=Group("", "", "")
-        app.group.create(group)
-        new_group=app.group.get_group_list()
-        assert len(old_group)+1 == len(new_group)
-        old_group.append(group)
-        assert sorted(old_group, key=Group.id_or_max)==sorted(new_group, key=Group.id_or_max)
+#def test_add_group_empty(app):
+       # old_group=app.group.get_group_list()
+     #   group=Group("", "", "")
+      #  app.group.create(group)
+       # new_group=app.group.get_group_list()
+       # assert len(old_group)+1 == len(new_group)
+       # old_group.append(group)
+       # assert sorted(old_group, key=Group.id_or_max)==sorted(new_group, key=Group.id_or_max)
 
 
 
