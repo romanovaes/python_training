02a960c59991c4f2cc73d3e7383652ba8923bbf2
Romanova Ekaterina <lyynakat@gmail.com>
2019-09-04 20:44:16 +0300

    добавление передачи пути к конфигурационному файлу относительно conftest.py

---
+++ data/add_group.py
@@ -0,0 +1,12 @@
+from model.group import Group
+import random
+import string
+
+
+def random_string(prefix, maxlen):
+        symbols=string.ascii_letters+string.digits+string.punctuation+" "*10
+        return prefix+ "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata=[Group("", "", "")]+[
+        Group(name=random_string("name", 7), header=random_string("header", 5), footer=random_string("footer", 5))
+        for i in range(2)]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-def random_string(prefix, maxlen):
-        symbols=string.ascii_letters+string.digits+string.punctuation+" "*10
-        return prefix+ "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata=[Group("", "", "")]+[
-        Group(name=random_string("name", 7), header=random_string("header", 5), footer=random_string("footer", 5))
-        for i in range(2)]
+from data.add_group import testdata
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
