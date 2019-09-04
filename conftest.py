740b74cf77ccbd734f774a5aef3000b9ce66cfe8
Romanova Ekaterina <lyynakat@gmail.com>
2019-09-04 20:34:27 +0300

    добавление передачи пути к конфигурационному файлу относительно conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import pytest
+import  os.path
 from fixture.application import Application
 
 fixture=None
@@ -12,8 +13,9 @@ def app(request):
     global target
     browser=request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-           target=json.load(config_file)
+        config_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+           target=json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture=Application(browser=browser, baseUrl=target["baseUrl"])
     fixture.session.ensue_login(user_name=target["username"], password=target["password"])
