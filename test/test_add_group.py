52126a386176a91b5868563de5a643d57ed8092c
Romanova Ekaterina <lyynakat@gmail.com>
2019-09-05 20:29:46 +0300

    сохранение и загрузка парамметров из файла json

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-from data.add_group import testdata
+from data.groups import testdata
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
