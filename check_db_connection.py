d551f1805c957e71df4a85973a160ae1d678d5cc
Romanova <romanove.e.s@effective-group.ru>
2019-09-19 21:59:37 +0300

    отношение между объектами-добавление методов

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 connection = pymysql.connect(host="127.0.0.1", db="addressbook", user="root", password="")
 db=ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
@@ -13,7 +14,7 @@ db=ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
     #connection.close()
 
 try:
-   l=db.get_contact_list()
+   l=db.get_contacts_not_in_group(Group(id='4'))
    for item in l:
        print(item)
    print(len(l))
